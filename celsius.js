5003a978e4b2fc8546adbb0e85af9014c379b4fe
Kenneth Onu <kennetho.onu@gmail.com>
2022-11-15 21:50:15 +0800

    convert celsius temperature to fahrenheit

---
+++ celsius.js
@@ -0,0 +1,13 @@
+/*
+Write a program that asks for a temperature in Celsius degrees, then displays it in Fahrenheit degrees.
+
+The conversion between scales is given by the formula: [°F] = [°C] x 9/5 + 32.
+*/
+
+let celsius = prompt("What is today's temperature in Celsius?")
+
+let fahrenheit = celsius * 9/5 + 32
+
+return fahrenheit
+
+//console.log(fahrenheit)
